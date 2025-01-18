d8e8b893d3ab12bce522d37f331007a9a189061b
Renee Messersmith <morganrmessersmith@gmail.com>
2025-01-18 17:05:17 -0500

    Feat: implemented JWT tokens for login session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  has_many :routine, dependent: :destroy
+  # has_many :routine_poses, through: :routines
+
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/
+
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX, message: "must be a valid email address" }, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true
   validates :password, presence: { require: true }
   has_secure_password
+
 end
