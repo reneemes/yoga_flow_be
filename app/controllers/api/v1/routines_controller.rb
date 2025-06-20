c01fc3bbae57e8977601d915b1daa84d47ad4ac1
Renee Messersmith <morganrmessersmith@gmail.com>
2025-06-20 15:05:04 -0400

    Feat: changed seeds to save JSON poses to db, created a routine create endpoint, updated documentation

--- app/controllers/api/v1/routines_controller.rb
+++ app/controllers/api/v1/routines_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::RoutinesController < ApplicationController
-  rescue_from StandardError, with: :handle_standard_error
+  # rescue_from StandardError, with: :handle_standard_error
 
   def index
-    # all_routines = Routine.where(user_id: @current_user.id)
     all_routines = Routine.all
     render json: RoutineSerializer.new(all_routines)
   end
@@ -11,4 +10,33 @@ class Api::V1::RoutinesController < ApplicationController
     routine = Routine.find_by(id: params[:id])
     render json: RoutineSerializer.new(routine)
   end
+
+  def create
+    new_routine = Routine.create(routine_params)
+
+    if new_routine.save
+      handle_poses(params[:poses], new_routine)
+      return render json: RoutineSerializer.new(new_routine)
+    else
+      return render json: {"error occurred...": error}
+    end
+  end
+
+  private
+
+  def routine_params
+    params.require(:routine).permit(:name, :description, :difficulty, poses: [])
+  end
+
+  def handle_poses(api_ids, routine)
+    api_ids.each do |api_id|
+      pose = Pose.find_by(api_id: api_id)
+      if pose
+        RoutinePose.create(routine_id: routine.id, pose_id: pose.id)
+      else
+        Rails.logger.warn("Pose with api_id #{api_id} not found.")
+      end
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,42 +8,23 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# dolly = User.create!(name: "Dolly Parton", email: "dollyP@email.com", password: "Jolene123")
-routine = Routine.create!(
-  name: "Example Routine",
-  description: "This is an example routine for getting started",
-  difficulty: "Beginner",
-  # user_id: dolly.id,
-)
-routine2 = Routine.create!(
-  name: "Example Routine 2",
-  description: "This is another example routine for getting started",
-  difficulty: "Intermediate",
-)
-routine3 = Routine.create!(
-  name: "Example Routine 3",
-  description: "This is yet another example routine for getting started",
-  difficulty: "Advanced",
-)
-pose1 = Pose.create!(
-  id: 5,
-  name: "Butterfly",
-  sanskrit_name: "Baddha Konasana",
-  translation_name: "baddha = bound, koṇa = angle, āsana = posture",
-  description: "In sitting position, bend both knees and drop the knees to each side, opening the hips.  Bring the soles of the feet together and bring the heels as close to the groin as possible, keeping the knees close to the ground.  The hands may reach down and grasp and maneuver the feet so that the soles are facing upwards and the heels and little toes are connected.  The shoulders should be pulled back and no rounding of the spine.",
-  pose_benefits: "Opens the hips and groins.  Stretches the shoulders, rib cage and back.  Stimulates the abdominal organs, lungs and heart.",
-  image_url: "https://res.cloudinary.com/dko1be2jy/image/upload/fl_sanitize/v1676483074/yoga-api/5_i64gif.svg",
-  )
-pose2 = Pose.create!(
-  id: 10,
-  name: "Child's Pose",
-  sanskrit_name: "Balasana",
-  translation_name: "bala = child, āsana = posture",
-  description: "From a kneeling position, the toes and knees are together with most of the weight of the body resting on the heels of the feet.  The arms are extended back resting alongside the legs.  The forehead rests softly onto the earth.  The gaze is down and inward.",
-  pose_benefits: "Gently stretches the hips, thighs, and ankles.  Calms the brain and helps relieve stress and fatigue.  Relieves back and neck pain when done with head and torso supported.",
-  image_url: "https://res.cloudinary.com/dko1be2jy/image/upload/fl_sanitize/v1676483079/yoga-api/10_wzpo85.svg",
-)
-RoutinePose.create!(routine_id: routine.id, pose_id: pose1.id)
-RoutinePose.create!(routine_id: routine.id, pose_id: pose2.id)
-RoutinePose.create!(routine_id: routine2.id, pose_id: pose1.id)
-RoutinePose.create!(routine_id: routine2.id, pose_id: pose2.id)
+require 'csv'
+
+file_contents = File.read(Rails.root.join('public', 'data', 'all_poses.json')) 
+pose_json = JSON.parse(file_contents, symbolize_names: true)
+
+pose_json.each do |row|
+  begin
+    Pose.find_or_create_by!(
+      api_id: row[:id],
+      name: row[:english_name],
+      sanskrit_name: row[:sanskrit_name_adapted],
+      description: row[:pose_description],
+      pose_benefits: row[:pose_benefits],
+      translation_name: row[:translation_name],
+      image_url: row[:url_svg]
+    )
+  rescue => error
+    Rails.logger.error "Failed to create pose: #{row[:english_name]} - #{e.message}"
+  end
+end
