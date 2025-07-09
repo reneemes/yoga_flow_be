cc832c0a00a1631392e80418c5924b95ac369138
Renee Messersmith <morganrmessersmith@gmail.com>
2025-07-09 17:14:39 -0400

    Feat: skipped current user action in poses controller

--- app/serializers/poses_serializer.rb
+++ app/serializers/poses_serializer.rb
@@ -1,9 +1,6 @@
 class PosesSerializer
   include JSONAPI::Serializer
 
-  # attributes :english_name, :sanskrit_name, :pose_description, :pose_benefits,
-  #           :category, :difficulty, :translation_name, :image_url
-
   def self.format_poses(poses)
     poses = poses.map do |pose|
       {
@@ -13,11 +10,6 @@ class PosesSerializer
           "attributes": {
             "name": pose[:english_name],
             "sanskrit_name": pose[:sanskrit_name_adapted],
-            # "pose_description": pose[:pose_description],
-            # "pose_benefits": pose[:pose_benefits],
-            # "category": pose[:category],
-            # "difficulty": pose[:difficulty],
-            # "translation_name": pose[:translation_name],
             "image_url": pose[:url_svg]
           }
         }
@@ -35,31 +27,10 @@ class PosesSerializer
             "sanskrit_name": pose[:sanskrit_name],
             "pose_description": pose[:description],
             "pose_benefits": pose[:pose_benefits],
-            # "category": pose[:category],
-            # "difficulty": pose[:difficulty],
             "translation_name": pose[:translation_name],
             "image_url": pose[:image_url]
           }
         }
       }
   end
-
-  # def self.format_one_pose(pose)
-  #   {
-  #       "data": {
-  #         "id": pose[:id].to_s,
-  #         "type": "pose",
-  #         "attributes": {
-  #           "name": pose[:english_name],
-  #           "sanskrit_name": pose[:sanskrit_name_adapted],
-  #           "pose_description": pose[:pose_description],
-  #           "pose_benefits": pose[:pose_benefits],
-  #           # "category": pose[:category],
-  #           # "difficulty": pose[:difficulty],
-  #           "translation_name": pose[:translation_name],
-  #           "image_url": pose[:url_svg]
-  #         }
-  #       }
-  #     }
-  # end
 end
