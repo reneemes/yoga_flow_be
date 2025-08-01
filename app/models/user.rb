7c014ea10c2e3c9910774504875205093ee9dc2c
Renee Messersmith <morganrmessersmith@gmail.com>
2025-08-01 18:47:45 -0400

    Fix: database seeding error 2nd try

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :routine, dependent: :destroy
-  has_many :routine_poses, through: :routine
+  has_many :routines, dependent: :destroy
+  has_many :routine_poses, through: :routines
 
   VALID_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/
 
