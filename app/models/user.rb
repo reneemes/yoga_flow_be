a3889f386a96d83002a6020427e7539babcc4d5a
Renee Messersmith <morganrmessersmith@gmail.com>
2025-08-18 22:24:07 -0400

    Fix: typo in 'uniqueness' in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
-  has_many :routine, dependent: :destroy
-  has_many :routine_poses, through: :routine
+  has_many :routines, dependent: :destroy
+  has_many :routine_poses, through: :routines
 
   VALID_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/
 
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   validates :email, 
     presence: true,
     format: { with: VALID_EMAIL_REGEX, message: "must be a valid email address" },
-    uniqueness: {case_sensative: false}
-  validates :password, presence: { require: true }
+    uniqueness: { case_sensitive: false }
   has_secure_password
-
 end
